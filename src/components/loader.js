c87e0cc57f86d137422f7deea938984b213cb02b
kanya384 <kanya384@mail.ru>
2022-03-03 16:57:30 +0300

    initial commit

---
+++ src/components/loader.js
@@ -0,0 +1,67 @@
+import React, { Suspense, useEffect, useState, useContext } from "react"
+import { BlocksContext } from "../context/blocksContext"
+import { Popups } from "./popups";
+import { Menu } from './menu';
+import { Header } from './header';
+import { Genplan } from './genplan';
+import { Plan_price } from './plan_price';
+import { How_buy } from './how_buy';
+import { Galery } from './galery';
+import { Hod_str } from './hod_str';
+import { Contacts } from './contacts';
+import { Footer } from './footer';
+
+
+
+
+export const Loader = () => {
+
+    const blocksImports = [
+        <Header />,
+        <Menu />,
+        <Genplan />,
+        <Plan_price />,
+        <How_buy />,
+        <Galery />,
+        <Hod_str />,
+        <Contacts />,
+        <Footer />,
+    ]
+
+    const loaded = useContext(BlocksContext)
+
+    const LoadBlock = (block) => {
+        return <Suspense fallback={<div>Загрузка...</div>}>{blocksImports[block]}</Suspense>
+    }
+
+    const generateHtml = () => {
+        let toDraw = []
+        for (let i = 0; i < loaded.blocks; i++) {
+            toDraw.push(LoadBlock(blocksImports[i]))
+        }
+        return (
+            <div className="blocks" data={loaded.menuClick ? "true" : ""}>
+                <Popups />
+                {toDraw.map((block) => block)}
+            </div>
+        )
+    }
+    const handleScroll = (event) => {
+
+        if (loaded.blocks < Object.keys(blocksImports).length) {
+
+            loaded.setBlocks(20)
+            loaded.setPopup(true)
+
+        } else {
+            window.removeEventListener('scroll', handleScroll, true);
+        }
+    }
+    useEffect(() => {
+        if (loaded.blocks < Object.keys(blocksImports).length) {
+            window.addEventListener('scroll', handleScroll, true);
+        }
+    })
+    return generateHtml()
+
+}
