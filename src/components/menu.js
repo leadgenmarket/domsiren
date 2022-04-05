7b48bbb4aec9e1da7800e1e338123769ea41ec24
kanya384 <kanya384@mail.ru>
2022-04-05 13:50:49 +0300

    menu click

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,8 +1,9 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { BlocksContext } from "../context/blocksContext"
 
 export const Menu = () => {
     const blocks = useContext(BlocksContext)
+    const [opened, setOpened] = useState(false)
 
     const showPopup = (e) => {
         e.preventDefault()
@@ -26,9 +27,9 @@ export const Menu = () => {
     const clickMenu = (e) => {
         e.preventDefault()
         let popup = e.currentTarget.getAttribute("href")
-        if (blocks.blocks < 20) {
+        if (blocks.blocks < 13) {
             blocks.setMenuClick(true)
-            blocks.setBlocks(20)
+            blocks.setBlocks(13)
             setTimeout(() => {
                 window.scrollTo({
                     top: document.querySelector("." + popup).offsetTop,
@@ -37,16 +38,14 @@ export const Menu = () => {
                 setTimeout(() => {
                     blocks.setMenuClick(false)
                 }, 500)
-
-
             }, 1200)
-
         } else {
             window.scrollTo({
                 top: document.querySelector("." + popup).offsetTop,
                 behavior: "smooth"
             })
         }
+        setOpened(false)
     }
 
 
@@ -55,22 +54,27 @@ export const Menu = () => {
 
     }, [])
 
+    const mobileMenuClick = (e) => {
+        e.preventDefault()
+        setOpened(!opened)
+    }
+
     return (
         <section className="hl_top plr">
             <div className="wmain">
                 <div className="hl_top__l">
                     <div className="logo">дом<br />на сиреневой</div>
-                    <div className="btn_nav_mobile">
+                    <div className="btn_nav_mobile" onClick={mobileMenuClick}>
                         <span></span>
                         <span></span>
                         <span></span>
                     </div>
                 </div>
-                <div className="hl_nav">
-                    <a href="#">Ипотека</a>
-                    <a href="#">Планировки и цены</a>
-                    <a href="#">Инфраструктура</a>
-                    <a href="#">Контакты</a>
+                <div className={opened ? "hl_nav hl_nav__opened" : "hl_nav"}>
+                    <a href="infra" onClick={clickMenu}>Инфраструктура</a>
+                    <a href="flat" onClick={clickMenu}>Планировки и цены</a>
+                    <a href="ipoteka" onClick={clickMenu}>Ипотека</a>
+                    <a href="contact" onClick={clickMenu}>Контакты</a>
                 </div>
                 <a className="hl_phone roistat-phone" href="tel:84959880202"><img src="img/hl_phone_ico.png" />8 495 988 02 02</a>
             </div>
