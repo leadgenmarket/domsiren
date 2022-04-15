e1a76d07800daea9b56bc4da2af5d8941b53f79b
Sergey <serzhei@bk.ru>
2022-04-15 15:13:17 +0300

    edit verstka

--- src/components/header.js
+++ src/components/header.js
@@ -18,6 +18,12 @@ export const Header = () => {
                     млн. ₽
                 </div>
             </div>
+            <div className='header_center__plashka_main'>
+                <div className='header_center__plashka'>
+                    <span>Ипотека для всех <b>8,0%</b></span>
+                    <span>Семейная ипотека от <b>2,5%</b></span>
+                </div>
+            </div>
         </div>
     </div>
     )
